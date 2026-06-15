af3f7c1909f4e494f2946624b7fff225e8449edc
Satish Mhaske <smhaske@qti.qualcomm.com>
2026-06-15 22:40:29 +0000

    Upload artifacts to S3 with TAG
    Recently IT release a Tagging policy for S3, where every object needs to
    be tagged as per the retention policy, such as e.g. "retention=7d" for
    7 days of retention of artifacts on S3 bucket.
    
    Signed-off-by: Satish Mhaske <smhaske@qti.qualcomm.com>

--- publish_artifacts.sh
+++ publish_artifacts.sh
@@ -2,11 +2,33 @@
 set -e
 
 # Step 1: Check AWS credentials
-# if ! aws sts get-caller-identity >/dev/null 2>&1; then
-#   echo "AWS credentials are missing or invalid."
-#   exit 1
-# fi
+if ! aws sts get-caller-identity >/dev/null 2>&1; then
+  echo "AWS credentials are missing or invalid."
+  exit 1
+fi
 
 # Step 2: Upload artifacts
-aws s3 cp "$INPUT_PATH" "s3://$INPUT_S3_BUCKET/$INPUT_DESTINATION" --recursive
+#aws s3 cp "$INPUT_PATH" "s3://$INPUT_S3_BUCKET/$INPUT_DESTINATION" --recursive --progress-frequency 30
 
+# BUCKET=$INPUT_S3_BUCKET
+# PREFIX=$INPUT_DESTINATION
+# LOCAL_DIR=$INPUT_PATH
+# TAG=$INPUT_OBJECT_TAG
+
+# Loop through all files recursively
+find "$INPUT_PATH" -type f | while read file; do
+    # Remove base path to preserve relative structure
+    relative_path="${file#$INPUT_PATH/}"
+
+    echo "Uploading: $relative_path"
+
+    aws s3api put-object \
+        --bucket "$INPUT_S3_BUCKET" \
+        --key "$INPUT_DESTINATION$relative_path" \
+        --body "$file" \
+        --tagging "$INPUT_OBJECT_TAG"
+done
+
+# Step 3: Publish fileserver URL containing the artifacts
+output_file="${GITHUB_OUTPUT}"
+echo "url=${INPUT_FILESERVER_URL}/${INPUT_S3_BUCKET}/${INPUT_DESTINATION}" >> ${output_file}
